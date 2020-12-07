ccf6e0bd4a4919e82f033ae2c7324ac0fc2513c7
althafib <althaf@newagesmb.com>
2020-12-07 12:55:58 +0530

    route fixed

--- src/app/layouts/admin-layout/admin.routing.module.ts
+++ src/app/layouts/admin-layout/admin.routing.module.ts
@@ -5,49 +5,6 @@ import { DashboardComponent } from '../../dashboard/dashboard.component';
 
 export const AdminLayoutRoutes: Routes = [
 
-    {
-      path: 'dashboard',
-      children: [ {
-        path: 'dashboard',
-        component: DashboardComponent
-    }]}, 
-    // path: '',
-    // children: [ {
-    //   path: 'userprofile',
-    //   component: UserProfileComponent
-    // }]
-    // }, {
-    //   path: '',
-    //   children: [ {
-    //     path: 'icons',
-    //     component: IconsComponent
-    //     }]
-    // }, {
-    //     path: '',
-    //     children: [ {
-    //         path: 'notifications',
-    //         component: NotificationsComponent
-    //     }]
-    // }, {
-    //     path: '',
-    //     children: [ {
-    //         path: 'maps',
-    //         component: MapsComponent
-    //     }]
-    // }, {
-    //     path: '',
-    //     children: [ {
-    //         path: 'typography',
-    //         component: TypographyComponent
-    //     }]
-    // }, {
-    //     path: '',
-    //     children: [ {
-    //         path: 'upgrade',
-    //         component: UpgradeComponent
-    //     }]
-    // }
-    
-    { path: 'dashboard',      component: DashboardComponent },
-  
+  { path: 'dashboard', component: DashboardComponent },
+
 ];
